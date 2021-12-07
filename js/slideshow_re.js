47ca81715ff28819bc4aa74a27bf178ab19ae0d9
sunmichoi <90458271+sunmichoi@users.noreply.github.com>
2021-12-07 18:53:34 +0900

    delet all

--- js/slideshow_re.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){ 
-    $('.bxslider').bxSlider({
-        auto: true,
-        speed: 500,
-        pause: 3000,
-        autoHover: true,
-        touchEnabled: (navigator.maxTouchPoints > 0)
-    })
-});
